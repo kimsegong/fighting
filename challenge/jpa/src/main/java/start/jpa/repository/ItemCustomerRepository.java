fdcc53f0781977d9de81ca1f74cca1022ca0ccc1
unknown <kimsegong@naver.com>
2024-06-15 19:26:00 +0900

    add

--- challenge/jpa/src/main/java/start/jpa/repository/ItemCustomerRepository.java
+++ challenge/jpa/src/main/java/start/jpa/repository/ItemCustomerRepository.java
@@ -10,9 +10,9 @@ public interface ItemCustomerRepository {
 
 
   List<Item> findAll();
-  List<Item> findById(Long id);
-  @Query("UPDATE Item i SET i.name = :name, i.price = :price, i.amount = :amount, i.status = :status WHERE i.id = :id")
-  Item modifyItem(Long id, String name);
-  @Query("DELETE Item i WHERE i.id =:id")
-  int deleteItem(Long id);
+//  List<Item> findById(Long id);
+//  @Query("UPDATE Item i SET i.name = :name, i.price = :price, i.amount = :amount, i.status = :status WHERE i.id = :id")
+//  Item modifyItem(Long id, String name);
+//  @Query("DELETE Item i WHERE i.id =:id")
+//  int deleteItem(Long id);
 }
